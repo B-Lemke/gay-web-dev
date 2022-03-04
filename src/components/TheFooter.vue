4626def4c245c0e40973d0632d53513360328132
Broderick Lemke <blemke4@gmail.com>
2022-03-04 13:12:27 -0600

    Creation of project and adding in scss.

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,78 @@
+<template>
+  <footer class="bg-dark text-light row w-100 py-5 px-4 mx-0">
+    <div
+      class="col-12 col-lg-2 d-flex flex-column justify-content-between px-1 mb-4 text-center text-lg-left"
+    >
+      <i class="fas fa-heart footer-heart"></i>
+      <div>
+        <small
+          >&copy; Broderick Lemke
+          <span class="current-year-replace">2020</span></small
+        >
+      </div>
+    </div>
+    <div class="col-6 col-lg-2 d-flex flex-column justify-content-center mb-4">
+      <ul class="m-0 list-unstyled">
+        <li><a href="#">About Me</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Blog</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-lg-2 d-flex flex-column justify-content-center mb-4">
+      <ul class="m-0 list-unstyled">
+        <li>
+          <a href="#"><i class="fab fa-github mr-2 text-info"></i>Github</a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-twitter mr-2 text-info"></i>Twitter</a>
+        </li>
+        <li>
+          <a href="#"
+            ><i class="fab fa-linkedin-in mr-2 text-info"></i>LinkedIn</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="col-lg-3 d-flex flex-column justify-content-center mb-4">
+      <label for="subscribeToBlog">Subscribe to my blog</label>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control footer-email-dark font-italic replace-placeholder-email"
+          placeholder="mscott@dminfinity.net"
+          aria-label="Subscribe to my blog"
+          aria-describedby="footerSubscribeToBlog"
+        />
+        <div class="input-group-append">
+          <button
+            class="btn btn-outline-light footer-email-button"
+            type="button"
+            id="footerSubscribeToBlog"
+          >
+            Ok!
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 d-flex flex-column justify-content-center">
+      <div>
+        <p class="m-0"><strong>Like What You See?</strong></p>
+        <p class="mb-1">
+          I'm always happy to talk with potential clients, employers, and
+          colleagues. Please reach out to me at:
+        </p>
+        <p class="m-0">
+          <i class="fas fa-envelope mr-2 text-primary"></i
+          >website-email@email.com
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {},
+};
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <header>
+    <nav
+      class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between"
+    >
+      <a class="navbar-brand gwd-logo" href="#">
+        <div id="logo-g">G</div>
+        <div id="logo-a" class="invis-at-first">A</div>
+        <div id="logo-y" class="invis-at-first">Y</div>
+        <div id="logo-w">W</div>
+        <div id="logo-e" class="invis-at-first">E</div>
+        <div id="logo-b" class="invis-at-first">B</div>
+        <div id="logo-d">D</div>
+        <div id="logo-e2" class="invis-at-first">E</div>
+        <div id="logo-v" class="invis-at-first">V</div>
+        <p>Broderick Lemke</p>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item mr-3 text-light text-center">
+            <a class="nav-link styled-link" href="www.google.com">About</a>
+          </li>
+          <li class="nav-item mr-3 text-center">
+            <a class="nav-link styled-link" href="www.google.com">Blog</a>
+          </li>
+          <li class="nav-item mr-3 text-center">
+            <a class="nav-link styled-link" href="www.google.com">Portfolio</a>
+          </li>
+          <li class="nav-item mr-3 text-center">
+            <a class="nav-link styled-link" href="www.google.com">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {},
+};
+</script>
