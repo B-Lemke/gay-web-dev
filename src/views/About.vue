851351de5bde0b3acc15074f2c25ded2d5a82d36
Broderick Lemke <blemke4@gmail.com>
2024-06-09 15:03:57 -0500

    About Page Timeline!

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,81 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <ul class="timeline">
+      <li class="primary">
+        <div style="--accent-color: #41516c" class="timeline-title">
+          2009 - 2014
+        </div>
+        <div class="timeline-body row w-100 mx-auto">
+          <div class="col-md-3">
+            <img
+              src="@/assets/images/tobby/tobbySquare2.jpg"
+              alt="Tobby near a tower"
+              class="hoverPic w-100 m-auto rounded-circle"
+            />
+          </div>
+          <div class="col-md-9">Description</div>
+        </div>
+      </li>
+      <li class="secondary">
+        <div style="--accent-color: #41516c" class="timeline-title">
+          2014 - 2017
+        </div>
+        <div class="timeline-body row w-100 mx-auto">
+          <div class="col-md-3">
+            <img
+              src="@/assets/images/tobby/tobbySquare2.jpg"
+              alt="Tobby near a tower"
+              class="hoverPic w-100 m-auto rounded-circle"
+            />
+          </div>
+          <div class="col-md-9">Description</div>
+        </div>
+      </li>
+      <li class="info">
+        <div style="--accent-color: #41516c" class="timeline-title">
+          2017 - 2019
+        </div>
+        <div class="timeline-body row w-100 mx-auto">
+          <div class="col-md-3">
+            <img
+              src="@/assets/images/tobby/tobbySquare2.jpg"
+              alt="Tobby near a tower"
+              class="hoverPic w-100 m-auto rounded-circle"
+            />
+          </div>
+          <div class="col-md-9">Description</div>
+        </div>
+      </li>
+      <li class="primary">
+        <div style="--accent-color: #41516c" class="timeline-title">2019</div>
+        <div class="timeline-body row w-100 mx-auto">
+          <div class="col-md-3">
+            <img
+              src="@/assets/images/tobby/tobbySquare2.jpg"
+              alt="Tobby near a tower"
+              class="hoverPic w-100 m-auto rounded-circle"
+            />
+          </div>
+          <div class="col-md-9">Description</div>
+        </div>
+      </li>
+      <li class="secondary">
+        <div style="--accent-color: #41516c" class="timeline-title">2021</div>
+        <div class="timeline-body row w-100 mx-auto">
+          <div class="col-md-3">
+            <img
+              src="@/assets/images/tobby/tobbySquare2.jpg"
+              alt="Tobby near a tower"
+              class="hoverPic w-100 m-auto rounded-circle"
+            />
+          </div>
+          <div class="col-md-9">Description</div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
+
+<style scoped></style>
